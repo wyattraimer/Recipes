ef10f0b0a8c143ffebf82ead87ef9613270bb09e
Wyatt <wyattraimer@gmail.com>
2026-03-11 09:30:19 -0600

    Add app splash screen with loading animation and hide functionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const hideAppSplash = () => {
+  const splash = document.getElementById('app-splash')
+  if (!splash) return
+
+  requestAnimationFrame(() => {
+    splash.setAttribute('data-hidden', 'true')
+    setTimeout(() => splash.remove(), 260)
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+hideAppSplash()
